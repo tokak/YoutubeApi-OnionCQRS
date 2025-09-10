1c09cdb0c7ae9bc76d01748546d222bf740e51f0
Murat Tokak <tokakmurat01@gmail.com>
2025-09-10 19:25:35 +0300

    Dbcontext oluşturuldu örnek veriler eklendi

--- Core/YoutubeApi.Domain/Common/EntityBase.cs
+++ Core/YoutubeApi.Domain/Common/EntityBase.cs
@@ -3,7 +3,7 @@
 public class EntityBase:IEntityBase
 {
     public int Id { get; set; }
-    public DateTime CreatedDate { get; set; } = DateTime.Now;
+    public DateTime CreatedDate { get; set; }
     public bool IsDeleted { get; set; } = false;
 
 }
