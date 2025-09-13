7b32f068c217ed309ae95a004b99b9a7d0796c3c
Murat Tokak <tokakmurat01@gmail.com>
2025-09-13 22:38:10 +0300

    Mediatr ile product listesini çekme

---
+++ Core/YoutubeApi.Application/Registration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace YoutubeApi.Application
+{
+    //  uygulamadaki servis kayıtlarını topluca yapmak için kullanılan yardımcı sınıf
+    public static class Registration
+    {
+        // Extension method: IServiceCollection üzerine eklenebilen bir metot
+        // Program.cs veya Startup.cs içinde kolayca çağrılabilir
+        public static void AddApplication(this IServiceCollection services)
+        {
+            // Şu an çalışmakta olan assembly'i (derlenmiş projeyi) alıyoruz
+            var assembly = Assembly.GetExecutingAssembly();
+
+            // MediatR servislerini dependency injection container'a ekliyoruz
+            // Ve bu assembly içindeki tüm IRequestHandler, INotificationHandler vb. sınıfları otomatik kaydediyoruz
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assembly));
+        }
+    }
+
+}
