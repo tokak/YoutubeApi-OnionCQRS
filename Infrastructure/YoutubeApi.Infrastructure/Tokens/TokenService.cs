2660f563d158e89be7d353a6fff070690f6addc7
Murat Tokak <tokakmurat01@gmail.com>
2025-09-25 04:45:24 +0300

    Token service yapıldı, createtoken metodu yapılandırıldı.

---
+++ Infrastructure/YoutubeApi.Infrastructure/Tokens/TokenService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using YoutubeApi.Application.Interfaces.Tokens;
+using YoutubeApi.Domain.Entities;
+
+namespace YoutubeApi.Infrastructure.Tokens
+{
+    public class TokenService : ITokenService
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly TokenSettings _tokenSettings;
+
+        public TokenService(UserManager<User> userManager, TokenSettings tokenSettings)
+        {
+            _userManager = userManager;
+            _tokenSettings = tokenSettings;
+        }
+
+        public async Task<JwtSecurityToken> CreateToken(User user, IList<string> roles)
+        {
+            var claims = new List<Claim>()
+            {
+              new  Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+              new Claim(ClaimTypes.NameIdentifier,user.Id.ToString()),
+              new Claim(JwtRegisteredClaimNames.Email,user.Email)
+            };
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_tokenSettings.Secret));
+            var token = new JwtSecurityToken(
+                issuer: _tokenSettings.Issuer,
+                audience: _tokenSettings.Audience,
+                expires: DateTime.Now.AddMinutes(_tokenSettings.TokenValidityInMunitues),
+                claims: claims,
+                signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256));
+
+            await _userManager.AddClaimsAsync(user,claims);
+
+            return token;
+        }
+
+        public string GenerateRefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClaimsPrincipal GetPrincipalFromExpriredToken()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
